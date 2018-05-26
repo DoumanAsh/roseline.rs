aad3d7d4a7b606dc6285fdbc340c9e5a4cb9e452
Douman <douman@gmx.se>
2018-05-26 15:52:28 +0300

    Revert "Don't use cross on regular linux build"
    This reverts commit 7bd63ac41a734d114f94ec6aaaa64d5fa4d399a5.

--- ci/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-    cargo --version
-else
-    cargo install cross || true
-    cross --version
-fi
--- ci/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-    cargo test
-else
-    cross test --target $TARGET
-fi
