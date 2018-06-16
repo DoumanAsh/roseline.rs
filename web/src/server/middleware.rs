11a9188d3c63b371aed6294765546ae2ff8fa760
Douman <douman@gmx.se>
2018-06-16 11:35:48 +0300

    Implement own logger middleware

--- web/src/server/middleware.rs
+++ web/src/server/middleware.rs
@@ -4,8 +4,7 @@ extern crate bytes;
 
 use self::http::header;
 use self::actix_web::{HttpRequest, Result, HttpResponse};
-use self::actix_web::middleware::{Middleware, Response};
-pub use self::actix_web::middleware::{Logger};
+use self::actix_web::middleware::{Middleware, Response, Started, Finished};
 
 ///Default headers middleware
 pub struct DefaultHeaders;
@@ -30,5 +29,50 @@ impl<S> Middleware<S> for DefaultHeaders {
         }
 
         Ok(Response::Done(resp))
+    }
+}
+
+///Debug only logger.
+///
+///Prints to info category but disabled unless `debug_assertions` are on
+pub struct Logger;
+
+impl<S> Middleware<S> for Logger {
+    #[allow(unused)]
+    fn start(&self, req: &mut HttpRequest<S>) -> Result<Started> {
+        #[cfg(debug_assertions)]
+        {
+            use self::actix_web::HttpMessage;
+            use ::std::str;
+
+            info!("HTTP: {remote} --> {method} {path} {version:?}\n{headers}\n",
+                  remote=req.connection_info().remote().unwrap_or(""),
+                  method=req.method(),
+                  path=req.path(),
+                  version=req.version(),
+                  headers=req.headers().iter()
+                                       .map(|(key, value)| format!("{}: {}\n", key.as_str(), str::from_utf8(value.as_bytes()).unwrap_or("Invalid UTF-8 value")))
+                                       .collect::<String>()
+                  );
+        }
+        Ok(Started::Done)
+    }
+
+    #[allow(unused)]
+    fn finish(&self, _req: &mut HttpRequest<S>, resp: &HttpResponse) -> Finished {
+        #[cfg(debug_assertions)]
+        {
+            use ::std::str;
+            info!("HTTP: <-- {version:?} {status}{error}\n{headers}\n",
+                  version=resp.version().unwrap_or(actix_web::http::Version::HTTP_11),
+                  status=resp.status(),
+                  error=resp.error().map(|error| format!(" Origin Error: {}", error)).unwrap_or("".to_string()),
+                  headers=resp.headers().iter()
+                                        .map(|(key, value)| format!("{}: {}\n", key.as_str(), str::from_utf8(value.as_bytes()).unwrap_or("Invalid UTF-8 value")))
+                                        .collect::<String>()
+                  );
+        }
+
+        Finished::Done
     }
 }
--- web/src/server/mod.rs
+++ web/src/server/mod.rs
@@ -240,8 +240,8 @@ fn db_dump(_: HttpRequest<AppState>) -> actix_web::Either<actix_web::fs::NamedFi
 }
 
 fn application(state: AppState) -> App<AppState> {
-    App::with_state(state).middleware(middleware::Logger::default())
-                          .middleware(middleware::DefaultHeaders)
+    App::with_state(state).middleware(middleware::DefaultHeaders)
+                          .middleware(middleware::Logger)
                           .resource("/", |res| {
                               res.method(Method::GET).h(templates::Index::new("/search", "Search AGTH Hook"));
                               res.route().f(not_allowed);
